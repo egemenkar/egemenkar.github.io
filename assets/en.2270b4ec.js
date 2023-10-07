5a94e5d9a50a1361b7126e00979dcb16a1ccf312
Egemen <egemenkar@gmail.com>
2023-10-07 21:04:23 +0300

    Updates

---
+++ assets/en.2270b4ec.js
@@ -0,0 +1,1 @@
+const t={title:{t:0,b:{t:2,i:[{t:3}],s:"My Personal Website"}}};export{t as default};
---
+++ assets/tr.93f8fa7c.js
@@ -0,0 +1,1 @@
+const t={title:{t:0,b:{t:2,i:[{t:3}],s:"Kişisel Web Sitem"}}};export{t as default};
